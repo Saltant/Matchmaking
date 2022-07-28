630489108df125d4e97e85e1e69b60d09182507a
Saltant Home © <saltant@inbox.ru>
2022-07-29 00:22:48 +0300

    Separate classes

---
+++ Extensions/ConcurrentQueueExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Saltant.Matchmaking.Models;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+
+namespace Saltant.Matchmaking
+{
+    public static class ConcurrentQueueExtensions
+    {
+        public static ConcurrentQueue<ThunderdomeTeam> TrackTeamCooldown(this ConcurrentQueue<ThunderdomeTeam> teams, Rank rank)
+        {
+            TimeTracker.UpdateThunderdomeTeamsTrack(teams, rank);
+
+            return teams;
+        }
+        public static void Update(this ConcurrentQueue<ThunderdomeTeam> teams, List<string> playersToRemove)
+        {
+            List<ThunderdomeTeam> temp = new();
+            foreach (var team in teams.Where(x => !x.IsElapsed && !playersToRemove.Contains(x.GetPlayerName))) temp.Add(team);
+            teams.Clear();
+            temp.ForEach((team) => teams.Enqueue(team));
+            temp.Clear();
+            temp = null;
+        }
+    }
+}
---
+++ TimeTracker.cs
@@ -0,0 +1,99 @@
+﻿using Saltant.Matchmaking.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Threading;
+
+namespace Saltant.Matchmaking
+{
+    public static class TimeTracker
+    {
+        static readonly Timer trackerTimer;
+        static ConcurrentQueue<ThunderdomeTeam> thunderdomeTrainingTeams;
+        static ConcurrentQueue<ThunderdomeTeam> thunderdomeRookieTeams;
+        static ConcurrentQueue<ThunderdomeTeam> thunderdomeProTeams;
+        static ConcurrentQueue<ThunderdomeTeam> thunderdomeVeteranTeams;
+        static readonly object locker = new();
+        static readonly List<string> playersToRemove = new();
+        static public event Action<ThunderdomeTeam> OnElapsedTeam;
+        static bool isUpdateRunning;
+        static TimeTracker()
+        {
+            if(trackerTimer == null)
+            {
+                trackerTimer = new Timer(Update, null, 0, 1000);
+            }
+        }
+
+        static void Update(object state)
+        {
+            if (isUpdateRunning) return;
+            isUpdateRunning = true;
+
+            if(thunderdomeTrainingTeams != null)
+            {
+                foreach (var elapsedTeam in thunderdomeTrainingTeams.Where(x => x.IsElapsed))
+                {
+                    OnElapsedTeam?.Invoke(elapsedTeam);
+                }
+                thunderdomeTrainingTeams.Update(playersToRemove);
+            }
+
+            if(thunderdomeRookieTeams != null)
+            {
+                foreach (var elapsedTeam in thunderdomeRookieTeams.Where(x => x.IsElapsed))
+                {
+                    OnElapsedTeam?.Invoke(elapsedTeam);
+                }
+                thunderdomeRookieTeams.Update(playersToRemove);
+            }
+
+            if(thunderdomeProTeams != null)
+            {
+                foreach (var elapsedTeam in thunderdomeProTeams.Where(x => x.IsElapsed))
+                {
+                    OnElapsedTeam?.Invoke(elapsedTeam);
+                }
+                thunderdomeProTeams.Update(playersToRemove);
+            }
+
+            if(thunderdomeVeteranTeams != null)
+            {
+                foreach (var elapsedTeam in thunderdomeVeteranTeams.Where(x => x.IsElapsed))
+                {
+                    OnElapsedTeam?.Invoke(elapsedTeam);
+                }
+                thunderdomeVeteranTeams.Update(playersToRemove);
+            }
+
+            playersToRemove.Clear();
+            isUpdateRunning = false;
+        }
+
+        internal static void UpdateThunderdomeTeamsTrack(ConcurrentQueue<ThunderdomeTeam> teams, Rank rank)
+        {
+            switch (rank)
+            {
+                case Rank.Training:
+                    thunderdomeTrainingTeams = teams;
+                    break;
+                case Rank.Rookie:
+                    thunderdomeRookieTeams = teams;
+                    break;
+                case Rank.Professional:
+                    thunderdomeProTeams = teams;
+                    break;
+                case Rank.Veteran:
+                    thunderdomeVeteranTeams = teams;
+                    break;
+            }
+        }
+
+        internal static void Remove(string playerName)
+        {
+            lock(locker)
+            if (!playersToRemove.Contains(playerName)) playersToRemove.Add(playerName);
+        }
+    }
+}
